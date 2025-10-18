a430074a53b5a6880de37ebd067ec0992a436396
肖文龙 <joel.xiao@dclingcloud.com>
2025-10-19 01:14:52 +0800

    feat(react-bridge): add 3D Portfolio Root Page

--- src/react/react-vue-plugin.ts
+++ src/react/react-vue-plugin.ts
@@ -1,9 +1,13 @@
 import type { App } from 'vue'
-import TestReact from './components/TestReact'
+import React from 'react'
+import Portfolio3D from './3d-portfolio/app/page'
+import PageContainer from './3d-portfolio/app/page-container'
 import { reactToVue } from './utils/react-to-vue'
 
 export default {
   install(app: App) {
-    app.component('TestReact', reactToVue(TestReact))
+    const CombinedComponent = () =>
+      React.createElement(PageContainer, null, React.createElement(Portfolio3D))
+    app.component('Portfolio3D', reactToVue(CombinedComponent))
   },
 }
